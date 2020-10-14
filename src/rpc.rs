f4dfa2e5cf2ca7227d206862369e86e1fd48c8d4
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2020-10-14 16:10:12 +0800

    domain types with a patched tendermint-rs

--- src/rpc.rs
+++ src/rpc.rs
@@ -3,20 +3,15 @@
 // TODO: docs for everything
 #![allow(missing_docs)]
 
-use prost::{decode_length_delimiter, Message};
-use std::convert::{TryFrom, TryInto};
+use prost::Message;
+use std::convert::TryFrom;
 use std::io::{self, Error, ErrorKind, Read};
 use tendermint::proposal::{SignProposalRequest, SignedProposalResponse};
 use tendermint::public_key::{PubKeyRequest, PublicKey};
 use tendermint::vote::{SignVoteRequest, SignedVoteResponse};
 use tendermint_proto::{
-    privval::{
-        message::Sum, Message as PrivMessage, PingRequest, PingResponse, RemoteSignerError,
-        SignProposalRequest as RawSignProposalRequest, SignVoteRequest as RawSignVoteRequest,
-    },
-    types::{
-        CanonicalBlockId, CanonicalPartSetHeader, CanonicalProposal, CanonicalVote, SignedMsgType,
-    },
+    privval::{message::Sum, Message as PrivMessage, PingRequest, PingResponse, RemoteSignerError},
+    types::SignedMsgType,
 };
 
 /// Maximum size of an RPC message
@@ -26,8 +21,8 @@ pub const MAX_MSG_LEN: usize = 1024;
 #[derive(Debug)]
 pub enum Request {
     /// Sign the given message
-    SignProposal(RawSignProposalRequest),
-    SignVote(RawSignVoteRequest),
+    SignProposal(SignProposalRequest),
+    SignVote(SignVoteRequest),
     ShowPublicKey(PubKeyRequest),
 
     // PingRequest is a PrivValidatorSocket message to keep the connection alive.
@@ -92,108 +87,62 @@ impl Request {
         let mut buf = vec![0; MAX_MSG_LEN];
         let bytes_read = r.read(&mut buf)?;
 
-        // FIXME: PrivMessage::decode_length_delimited was returning "unexpected VarInt"
-        let len = decode_length_delimiter(buf[..bytes_read].as_ref())?;
-        let start = bytes_read - len;
-
-        let req = PrivMessage::decode(&buf[start..bytes_read])?; //PrivMessage::decode_length_delimited(&buf[0..bytes_read])?;
+        let req = PrivMessage::decode_length_delimited(&buf[0..bytes_read])?;
         let err = Error::new(ErrorKind::InvalidData, "Invalid request.");
         match req.sum {
             Some(Sum::PubKeyRequest(pkr)) => {
-                let pkr = PubKeyRequest::try_from(pkr).map_err(|e| err)?;
+                let pkr = PubKeyRequest::try_from(pkr).map_err(|_e| {
+                    // dbg!(_e);
+                    err
+                })?;
                 Ok(Request::ShowPublicKey(pkr))
             }
             Some(Sum::PingRequest(pr)) => Ok(Request::ReplyPing(pr)),
             Some(Sum::SignVoteRequest(svr)) => {
-                // let svr = SignVoteRequest::try_from(svr).map_err(|e| {
-                //     err
-                // })?;
+                let svr = SignVoteRequest::try_from(svr).map_err(|_e| {
+                    // dbg!(_e);
+                    err
+                })?;
                 Ok(Request::SignVote(svr))
             }
             Some(Sum::SignProposalRequest(spr)) => {
-                // let spr = SignProposalRequest::try_from(spr).map_err(|e| {
-                //     err
-                // })?;
+                let spr = SignProposalRequest::try_from(spr).map_err(|_e| {
+                    // dbg!(_e);
+                    err
+                })?;
                 Ok(Request::SignProposal(spr))
             }
-            x => {
-                dbg!(x);
+            _x => {
+                // dbg!(_x);
                 Err(err)
             }
         }
     }
 }
 
-impl TendermintRequest for RawSignVoteRequest {
+impl TendermintRequest for SignVoteRequest {
     fn consensus_state(&self) -> (SignedMsgType, tendermint::consensus::State) {
-        // let mut state = self.vote.consensus_state();
-        // let smtype = if self.vote.is_precommit() {
-        //     state.step = 2;
-        //     SignedMsgType::Precommit
-        // } else {
-        //     state.step = 1;
-        //     SignedMsgType::Prevote
-        // };
-        // (smtype, state)
-
-        let vote = self.vote.as_ref().expect("fixme");
-        let vtype = vote.r#type();
-        (
-            vtype,
-            tendermint::consensus::State {
-                height: tendermint::block::Height::try_from(vote.height).expect("fixme"),
-                round: tendermint::block::Round::try_from(vote.round).expect("fixme"),
-                step: match vtype {
-                    SignedMsgType::Precommit => 2,
-                    SignedMsgType::Prevote => 1,
-                    _ => unreachable!("unknown vote"),
-                },
-                block_id: vote
-                    .block_id
-                    .as_ref()
-                    .map(|x| tendermint::block::Id::try_from(x.clone()).expect("fixme")),
-            },
-        )
+        let mut state = self.vote.consensus_state();
+        let smtype = if self.vote.is_precommit() {
+            state.step = 2;
+            SignedMsgType::Precommit
+        } else {
+            state.step = 1;
+            SignedMsgType::Prevote
+        };
+        (smtype, state)
     }
 
     fn height(&self) -> tendermint::block::Height {
-        // self.vote.height
-        self.vote
-            .as_ref()
-            .expect("fixme")
-            .height
-            .try_into()
-            .expect("fixme")
+        self.vote.height
     }
 
     fn set_signature(&mut self, signature: ed25519_dalek::Signature) {
-        let mut vote = self.vote.as_mut().expect("fixme");
-
-        vote.signature = signature.to_bytes().to_vec(); //tendermint::Signature::Ed25519(signature);
+        self.vote.signature = tendermint::Signature::Ed25519(signature);
     }
 
     fn to_sign_bytes(&self) -> Vec<u8> {
-        //self.to_signable_vec().expect("todo")
-
-        let mut r = vec![];
-        let vote = self.vote.as_ref().expect("fixme");
-        CanonicalVote {
-            r#type: vote.r#type,
-            height: vote.height,
-            round: vote.round as i64,
-            block_id: vote.block_id.as_ref().map(|x| CanonicalBlockId {
-                hash: x.hash.clone(),
-                part_set_header: x.part_set_header.as_ref().map(|y| CanonicalPartSetHeader {
-                    total: y.total,
-                    hash: y.hash.clone(),
-                }),
-            }),
-            timestamp: vote.timestamp.clone(),
-            chain_id: self.chain_id.clone(),
-        }
-        .encode_length_delimited(&mut r)
-        .expect("todo");
-        r
+        self.to_signable_vec().expect("todo")
     }
 
     fn build_response(self, error: Option<RemoteSignerError>) -> Response {
@@ -204,7 +153,7 @@ impl TendermintRequest for RawSignVoteRequest {
             }
         } else {
             SignedVoteResponse {
-                vote: Some(SignVoteRequest::try_from(self).expect("fixme").vote),
+                vote: Some(self.vote),
                 error: None,
             }
         };
@@ -213,62 +162,23 @@ impl TendermintRequest for RawSignVoteRequest {
     }
 }
 
-impl TendermintRequest for RawSignProposalRequest {
+impl TendermintRequest for SignProposalRequest {
     fn consensus_state(&self) -> (SignedMsgType, tendermint::consensus::State) {
-        // let mut state = self.proposal.consensus_state();
-        // state.step = 0;
-        let proposal = self.proposal.as_ref().expect("fixme");
-        (
-            SignedMsgType::Proposal,
-            tendermint::consensus::State {
-                height: tendermint::block::Height::try_from(proposal.height).expect("fixme"),
-                round: tendermint::block::Round::try_from(proposal.round).expect("fixme"),
-                step: 0,
-                block_id: proposal
-                    .block_id
-                    .as_ref()
-                    .map(|x| tendermint::block::Id::try_from(x.clone()).expect("fixme")),
-            },
-        )
+        let mut state = self.proposal.consensus_state();
+        state.step = 0;
+        (SignedMsgType::Proposal, state)
     }
 
     fn height(&self) -> tendermint::block::Height {
-        self.proposal
-            .as_ref()
-            .expect("fixme")
-            .height
-            .try_into()
-            .expect("fixme")
+        self.proposal.height
     }
 
     fn set_signature(&mut self, signature: ed25519_dalek::Signature) {
-        let mut proposal = self.proposal.as_mut().expect("fixme");
-
-        proposal.signature = signature.to_bytes().to_vec(); //tendermint::Signature::Ed25519(signature);
+        self.proposal.signature = tendermint::Signature::Ed25519(signature);
     }
 
     fn to_sign_bytes(&self) -> Vec<u8> {
-        let mut r = vec![];
-        let proposal = self.proposal.as_ref().expect("fixme");
-        CanonicalProposal {
-            r#type: proposal.r#type,
-            height: proposal.height,
-            round: proposal.round as i64,
-            pol_round: proposal.pol_round as i64,
-            block_id: proposal.block_id.as_ref().map(|x| CanonicalBlockId {
-                hash: x.hash.clone(),
-                part_set_header: x.part_set_header.as_ref().map(|y| CanonicalPartSetHeader {
-                    total: y.total,
-                    hash: y.hash.clone(),
-                }),
-            }),
-            timestamp: proposal.timestamp.clone(),
-            chain_id: self.chain_id.clone(),
-        }
-        .encode_length_delimited(&mut r)
-        .expect("todo");
-        r
-        // self.to_signable_vec().expect("todo")
+        self.to_signable_vec().expect("todo")
     }
 
     fn build_response(self, error: Option<RemoteSignerError>) -> Response {
@@ -279,7 +189,7 @@ impl TendermintRequest for RawSignProposalRequest {
             }
         } else {
             SignedProposalResponse {
-                proposal: Some(SignProposalRequest::try_from(self).expect("fixme").proposal),
+                proposal: Some(self.proposal),
                 error: None,
             }
         };
